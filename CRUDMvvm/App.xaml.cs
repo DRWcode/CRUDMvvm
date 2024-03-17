53854f180cc18a82e2608f28ac16548749597370
slowbrox <raudalesdarwin@gmail.com>
2024-03-17 17:36:53 -0600

    Add project files.

---
+++ CRUDMvvm/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUDMvvm
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
