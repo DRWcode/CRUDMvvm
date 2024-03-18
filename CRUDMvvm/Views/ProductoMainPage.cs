090a508aef4a5301b5fb18b154cb90570e622087
slowbrox <raudalesdarwin@gmail.com>
2024-03-17 20:37:28 -0600

    creacion clases y views

---
+++ CRUDMvvm/Views/ProductoMainPage.cs
@@ -0,0 +1,15 @@
+namespace CRUDMvvm.Views;
+
+public class ProductoMainPage : ContentPage
+{
+	public ProductoMainPage()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
